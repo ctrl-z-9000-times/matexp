a43e1ca8e6742d588943810cac01be0a8b8246b0
David McDougall <dam1784@rit.edu>
2026-04-02 13:59:15 -0400

    new file: figures/propagator_run.py

---
+++ figures/propagator_run.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+from pathlib import Path
+from subprocess import run
+import os
+
+figure_dir = Path(__file__).parent 
+program = figure_dir / "propagator_matrix.py"
+mod_files = figure_dir.glob("../mod/*.mod")
+out_dir = figure_dir / "propagator_out"
+out_dir.mkdir(exist_ok=True)
+cwd = Path.cwd()
+os.chdir(out_dir)
+
+for file in mod_files:
+	run([program, "-q", "--time_step", ".100", file])
+
+os.chdir(cwd)
