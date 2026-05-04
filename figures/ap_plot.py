6a608eb0b70239ea1852780cf9c14f74c2c2e9a5
David McDougall <dam1784@rit.edu>
2026-05-04 12:41:20 -0400

    results!

--- figures/ap_plot.py
+++ figures/ap_plot.py
@@ -35,7 +35,8 @@ for method, data in traces.items():
 # Setup the figure.
 fig = plt.figure("AP Demo", figsize=(7.5, 7.5))
 gs = fig.add_gridspec(2, 2, hspace=0, wspace=0)
-grid = gs.subplots(sharex='all', sharey='all')
+# grid = gs.subplots(sharex='all', sharey='all') # DEBUG ONLY (tick labels collide)
+grid = gs.subplots() # RELEASE
 titles = [
     "Matrix Exponential Method",
     "Backwards Euler Method",
@@ -43,9 +44,9 @@ titles = [
     "Approximate Matrix Exponential Method vs Accuracy",]
 methods = ["matexp", "sparse", "approx", "accuracy"]
 
-t_min, t_max = (3, 3.6)
+t_min, t_max = (3, 4)
 
-# Offest the time by 100 ms. (stimulus starts after 100 ms delay) 
+# Offset the time by 100 ms. (stimulus starts after 100 ms delay) 
 for method, data in traces.items():
     for value, (t, v) in data.items():
         t -= 100
@@ -65,9 +66,10 @@ for row, col, index in [(0, 0, 0), (0, 1, 1), (1, 0, 2), (1, 1, 3)]:
     axes = grid[row, col]
     title = titles[index]
     method = methods[index]
-    # 
-    axes.text(t_min+.05, 25, chr(ord("A") + index), ha='left', va='top',
+    # Sub-figure labels (A, B, C, D)
+    axes.text(t_min+.06, 27, chr(ord("A") + index), ha='left', va='top',
               size=16, weight="bold")
+    # 
     num_traces = len(traces[method])
     for trace_index, (value, (t, v)) in enumerate(traces[method].items()):
         assert all(np.isfinite(t))
@@ -75,18 +77,39 @@ for row, col, index in [(0, 0, 0), (0, 1, 1), (1, 0, 2), (1, 1, 3)]:
         if index in [0, 1, 2]:
             label = "Δt = %g"%float(value)
         elif index == 3:
-            label = f"max error = {value}"
-        # axes.plot(t, v, label=label)
-        axes.plot(t, v, label=label, color=cmc.batlow(trace_index / num_traces))
-    # axes.set_yticks([])
+            label = f"accuracy = {value}"
+        axes.plot(t, v, label=label, color=cmc.batlow(trace_index / 4))
     # axes.grid(which="major", axis='both', linestyle='solid', linewidth=1)
-    axes.set_xlabel("time (ms)")
     axes.set_xlim(xmin=t_min, xmax=t_max)
+    # Y-Axis labels & ticks
+    axes.yaxis.set_tick_params(direction="in")
+    if col == 0:
+        axes.set_ylabel("Membrane potential (mV)")
+        axes.set_yticks([-60, -40, -20, 0, 20])
+    elif col == 1:
+        axes.set_yticks([-60, -40, -20, 0, 20], labels=[""]*5)
+    # X-Axis labels & ticks
+    axes.xaxis.set_tick_params(direction="in")
+    if row == 0:
+        axes.set_xticks([3.0, 3.2, 3.4, 3.6, 3.8, 4.0], labels=[""]*6)
+    elif row == 1:
+        axes.set_xlabel("time (ms)")
+        if col == 0:
+            axes.set_xticks([3.0, 3.2, 3.4, 3.6, 3.8])
+        else:
+            axes.set_xticks([3.0, 3.2, 3.4, 3.6, 3.8, 4.0])
+    # Legend
     if index == 3:
         axes.legend(loc='center right')
-        axes.set_xticks([3.2, 3.4, 3.6])
     else:
-        axes.legend(loc='best')
+        axes.legend(loc='lower right')
+    # Hide the top & right borders
+    if index == 0:
+        axes.spines[['top']].set_visible(False)
+    elif index == 1:
+        axes.spines[['right', 'top']].set_visible(False)
+    elif index == 3:
+        axes.spines[['right']].set_visible(False)
 
 fig.savefig("ap_demo.png", dpi=600, bbox_inches='tight')
 if not os.environ.get('NOSHOW', ''): plt.show()
