f981a4f1bd51332999655b963779e29e71b7a336
David McDougall <dam1784@rit.edu>
2026-05-10 17:39:03 -0400

    fix: figures/gpu_sim.py

--- figures/gpu_plot.py
+++ figures/gpu_plot.py
@@ -7,18 +7,25 @@ import argparse
 import numpy as np
 import csv
 
+fieldnames = None
 measurements = []
 for file in sorted(Path("gpu_data").iterdir()):
     model = file.stem
     with open(file, 'rt') as f:
         reader = csv.DictReader(f)
+        if fieldnames is None:
+            fieldnames = reader.fieldnames
+        else:
+            assert fieldnames == reader.fieldnames
         for row in reader:
-            measurements.append((model, row["batch1000"], row["throughput"]))
+            data_tuple = (model,) + tuple(row[f] for f in fieldnames)
+            measurements.append(data_tuple)
 
 measurements.sort()
 
 with open("gpu_table.csv", 'wt') as f:
-    writer = csv.DictWriter(f, fieldnames=["model", "batch1000", "throughput"])
-    writer.writeheader()
-    for (model, batch1000, throughput) in measurements:
-        writer.writerow({"model": model, "batch1000": batch1000, "throughput": throughput})
+    fieldnames.insert(0, "model")
+    writer = csv.writer(f)
+    writer.writerow(fieldnames)
+    for row in measurements:
+        writer.writerow(row)
--- figures/gpu_sim.py
+++ figures/gpu_sim.py
@@ -44,12 +44,12 @@ def measure_speed(num_instances, continuous):
         conserve_sum=1.0,
         num_instances=num_instances)
     # Cycle through several different input values.
-    input_arrays = [[] for _ in range(20)]
+    input_arrays = [[] for _ in range(100)]
     for input_set in input_arrays:
         for inp in inputs:
             input_set.append(inp.random(num_instances, np.float64, cupy))
             input_set.append(cupy.arange(num_instances, dtype=np.int32))
-    input_iter = itertools.cycle(input_arrays)
+    input_iter = iter(input_arrays)
     # 
     def advance():
         inputs = next(input_iter)
@@ -57,15 +57,16 @@ def measure_speed(num_instances, continuous):
     # 
     start_event = cupy.cuda.Event()
     end_event   = cupy.cuda.Event()
+    # Always cold start
+    flush_cache()
     # 
     if continuous:
         num_steps = round(1 / time_step)
         # Warmup
-        for _ in range(5):
+        for _ in range(10):
             advance()
     else:
         num_steps = 1
-        flush_cache() # Cold start
     # Perform the measurement.
     start_event.record()
     for t in range(num_steps):
@@ -76,12 +77,15 @@ def measure_speed(num_instances, continuous):
     elapsed_ms = cupy.cuda.get_elapsed_time(start_event, end_event)
     return elapsed_ms
 # 
-batch_size = 1000
-batch_samples = []
-for _ in range(200):
-    batch_samples.append(measure_speed(batch_size, False))
-batch_time = min(batch_samples)
-print(f"batch {batch_size} x {batch_time} ms")
+batch_data = []
+for batch_size in [1, 10, 100, 1000, 10000, 100000]:
+    batch_samples = []
+    for _ in range(200):
+        batch_samples.append(measure_speed(batch_size, False))
+    batch_time = min(batch_samples)
+    batch_data.append((batch_size, batch_time))
+    print(f"batch {batch_size} x {batch_time} ms")
+batch_size, batch_time = zip(*batch_data)
 # 
 elapsed_ms      = 0
 num_instances   = 1000
@@ -106,5 +110,7 @@ print(f"throughput {num_instances} x")
 # 
 data_file = Path("gpu_data").joinpath(model_name).with_suffix('.csv')
 with open(data_file, 'wt') as file:
-    print(f"batch{batch_size},throughput", file=file)
-    print(batch_time, num_instances, sep=',', file=file)
+    header = list(batch_size) + ["throughput"]
+    row    = list(batch_time) + [num_instances]
+    print(','.join(header), file=file)
+    print(','.join(row), file=file)
